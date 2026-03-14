00b78dadf472f34d027f540f06104f02ee12eef5
Edgar <Edgar@AnotherFoxGuy.com>
2026-03-14 12:45:15 +0100

    :rocket: Added tool to export UPM package

--- Assets/ResoniteSDK Builder/Editor/ResoniteSDK_Builder.cs
+++ Assets/ResoniteSDK Builder/Editor/ResoniteSDK_Builder.cs
@@ -1,13 +1,17 @@
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class ResoniteSDK_Builder
 {
-    [MenuItem("Resonite SDK/Build UnityPackage")]
+    [MenuItem("Resonite SDK/Internal/Build UnityPackage")]
     public static void BuildUnityPackage()
     {
+        var version = "0.0.0";
+        JsonUtility.FromJsonOverwrite(File.ReadAllText("Assets/ResoniteSDK/package.json"), version);
+        
         var files = Directory.EnumerateFiles("Assets/ResoniteSDK/", "*", SearchOption.AllDirectories)
             .Where(file => Path.GetExtension(file) != ".meta").ToArray();
 
@@ -15,8 +19,25 @@ public class ResoniteSDK_Builder
 
         Directory.CreateDirectory("Builds");
 
-        AssetDatabase.ExportPackage(files, "Builds/ResoniteSDK.unitypackage", ExportPackageOptions.IncludeDependencies);
+        AssetDatabase.ExportPackage(files, $"Builds/ResoniteSDK-V{version}.unitypackage", ExportPackageOptions.IncludeDependencies);
 
         Debug.Log("ResoniteSDK UnityPackage built!");
+    }
+    
+    [MenuItem("Resonite SDK/Internal/Build UPM package")]
+    public static void BuildUpmPackage()
+    {
+        EditorUtility.DisplayProgressBar("ResoniteSDK","Building UPM package", 0);
+
+        var pk = Client.Pack("Assets/ResoniteSDK/", "Builds");
+        
+        while (!pk.IsCompleted) { }
+        
+        Debug.Log("ResoniteSDK UPM built!");  
+        
+        if (pk.Status == StatusCode.Failure)
+            Debug.LogError(pk.Error);
+
+        EditorUtility.ClearProgressBar();
     }
 }
